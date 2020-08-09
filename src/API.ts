8139dc792033c6d95c50b2ce9e44e66745899b8a
sukh <sukhveer.sangha@tatacommunications.com>
2020-08-09 16:10:15 +0100

    API.ts and utils complete, randomise questions func and map the data

--- src/API.ts
+++ src/API.ts
@@ -1,3 +1,14 @@
+import { shuffleArray } from './utils'
+
+export type Question = {
+    correct_answer: string
+    difficulty: string
+    incorrect_answers: string[]
+    question: string
+    type: string
+}
+
+export type QuestionState = Question & { answers: string[] }
 
 export enum Difficulty {
     EASY = 'easy',
@@ -8,5 +19,13 @@ export enum Difficulty {
 export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) => {
     const endpoint = `https://opentdb.com/api.php?amount=${amount}&category=21&difficulty=${difficulty}&type=multiple`
     const data = await (await fetch(endpoint)).json()
-    console.log(data)
+    return data.results.map((question: Question) => (
+        {
+            ...question,
+            answers: shuffleArray([
+                ...question.incorrect_answers, 
+                question.correct_answer
+            ])
+        }
+    ))
 }
