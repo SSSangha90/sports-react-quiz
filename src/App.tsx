79eb7b8b8b4e17d7ac80b4ebb40d96689de06a0a
sukh <sukhveer.sangha@tatacommunications.com>
2020-08-09 14:38:38 +0100

    built out the App component with buttons and set out event handler functions

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,39 @@ import QuestionCard from './components/QuestionCard'
 const React = require('react')
 
 const App = () => {
+
+  const startQuiz = async () => {
+
+  }
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  const nextQuestion = () => {
+
+  }
+
   return (
     <div>
-      <div className="App">Sports Quiz</div>
+      <div className="App">
+        <h1>Sports Quiz</h1>
+        <button 
+          className="start"
+          onClick={startQuiz}>
+            Start
+        </button>
+        <p className="score">Score:</p>
+        <p>Loading Questions...</p> {/** Add a Spinner */}
+      </div>
       <QuestionCard/>
+      <div>
+        <button 
+          className="next"
+          onClick={nextQuestion}>
+          Next Question
+        </button>
+      </div> 
     </div>
   )
 }
