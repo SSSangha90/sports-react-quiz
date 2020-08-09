a76b8e0ce5945519b0892a1be4ba2863123f1eee
sukh <sukhveer.sangha@tatacommunications.com>
2020-08-09 12:01:22 +0100

    fixed import issue using require instead

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+const React = require('react');
+const ReactDOM = require('react-dom');
 
 ReactDOM.render(
   <React.StrictMode>
