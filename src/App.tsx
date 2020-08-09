033cc2101d0d7286aedb865130ccd4d4d1c93bdc
sukh <sukhveer.sangha@tatacommunications.com>
2020-08-09 15:04:12 +0100

    used useState, react hook, to set the state

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import QuestionCard from './components/QuestionCard'
-const React = require('react')
+import React, {useState} from 'react'
 
 const App = () => {
+
+  const [loading, setLoading] = useState(false)
+  const [questions, setQuestions] = useState([])
+  const [number, setNumber] = useState(0)
+  const [userAnswers, setUserAnswers] = useState([])
+  const [score, setScore] = useState(0)
+  const [gameOver, setGameOver] = useState(true)
 
   const startQuiz = async () => {
 
